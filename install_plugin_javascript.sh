777ee677353d87ebd3984d2740369d968b41d6c2
Joel Ward <jmward@@gmail.com>
2015-03-22 21:42:25 -0700

    update

--- install_plugin_javascript.sh
+++ install_plugin_javascript.sh
@@ -1,1 +1,1 @@
-sh install_plugin.sh https://github.com/mulesoft/raml-client-generator raml-client-generator/languages/javascript javascript
+sh install_plugin.sh https://github.com/val314159/raml-client-generator raml-client-generator/languages/javascript javascript
--- languages/languages.js
+++ languages/languages.js
@@ -1,5 +1,4 @@
 // SEMI-AUTO-GENERATED FILE.  DONT EDIT. //prelude
 var Languagez = [ //prelude
- '',//language
  null]; //finale
 module.exports=Languagez; //finale
